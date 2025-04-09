6bd78ed7c0e74e130170f3bd46e7e94be892743e
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-04-08 22:59:45 -0300

    feature: teste de implementações de componentes

---
+++ src/components/Filter.vue
@@ -0,0 +1,4 @@
+<template>
+    <input type="text" name="pokemon filter" placeholder="Insira o pokemon">
+    <button>Pesquisar</button>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,16 @@
+<script>
+    import Filter from '../components/Filter.vue';
+    import PokemonCard from '../components/PokemonCard.vue';
+
+    export default {
+        components: {
+            PokemonCard,
+            Filter
+        }
+    }
+</script>
 <template>
     <h1>Componente principal</h1>
+    <Filter />
+    <PokemonCard />
 </template>
