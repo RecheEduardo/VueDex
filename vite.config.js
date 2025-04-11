5874ce7761878c8d8a48d7f84400e2f288b53e30
RecheEduardo <Eduardorechemartins2006@gmail.com>
2025-04-11 19:05:55 -0300

    feat: criando ambiente de testes automatizados com Vitest

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { configDefaults } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+        globals: true,         // usar describe/it sem importar
+        environment: 'jsdom',  // simula browser para os testes
+        exclude: [...configDefaults.exclude, 'cypress/*'],
+      }
 })
